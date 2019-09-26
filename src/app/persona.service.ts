3a00efe51874f7569c68c3f773c14431540a97e9
Javier Lete <javier@eskura21.com>
2019-09-26 20:59:34 +0200

    Servicio de personas

--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Persona } from './persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
+  personas: Persona[] = [
+    { id: 1, nombre: 'Javier Lete', email: 'javierlete@email.com' },
+    { id: 2, nombre: 'Pepe Pérez', email: 'pepeperez@email.com' }
+  ];
+
   constructor() { }
+
+  getPersonas(): Observable<Persona[]> {
+    return of(this.personas);
+  }
 }
