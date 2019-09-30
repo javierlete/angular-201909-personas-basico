48cf78373232c052fd69f42908c464f294159b4f
Javier Lete <javier@eskura21.com>
2019-09-30 19:18:41 +0200

    Ruta a persona

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListadoComponent } from './listado/listado.component';
+import { PersonaComponent } from './persona/persona.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'listado', pathMatch: 'full' },
-  { path: 'listado', component: ListadoComponent }
+  { path: 'listado', component: ListadoComponent },
+  { path: 'persona', component: PersonaComponent }
 ];
 
 @NgModule({
