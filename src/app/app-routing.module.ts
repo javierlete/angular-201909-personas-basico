605a392aaa7194eb1d287e2b9d7c01b32d1827ef
Javier Lete <javier@eskura21.com>
2019-09-30 19:15:39 +0200

    Rutado con /listado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listado', component: ListadoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
