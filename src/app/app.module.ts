a33359ef9e51eaf80d7191d7f316733545755a59
Javier Lete <javier@eskura21.com>
2019-10-01 18:53:53 +0200

    Añadir módulo de HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ListadoComponent } from './listado/listado.component';
 import { PersonaComponent } from './persona/persona.component';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
